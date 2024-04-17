cfe840c8a6174ebdba425d146c9a710efc57e39c
Ruben <ruben_picos10@hotmail.com>
2024-04-17 17:05:17 +0200

    First API call

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import Poster from "./components/Poster";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [titles, setTitles] = useState([]);
+  useEffect(() => {
+    const movies = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYmEwOTEyMjA5NmZkM2Y2ODY3N2VkNTVmZDc0YjhhMyIsInN1YiI6IjY1ZDM2MGFmZTA0ZDhhMDE2Mzk4YWI0MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2u9B1noWUTd7Y0nQQ6mw4t5fsBNMcExAXgW97P5ce0Y",
+      },
+    };
 
+    fetch(
+      "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+      movies
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        const movieTitles = response.results.slice(0, 8).map((result) => {
+          return result.original_title;
+        });
+        setTitles(movieTitles);
+      })
+      .catch((error) => console.error(error));
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Movies</h1>
+      <ol>
+        {titles.map((title, index) => (
+          <li key={index}>{title}</li>
+        ))}
+      </ol>
+      <Poster />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
